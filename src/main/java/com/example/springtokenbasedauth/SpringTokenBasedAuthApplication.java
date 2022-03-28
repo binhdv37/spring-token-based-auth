c66f977505a8dfae5e6a095e9235789b87f77e26
Doan Van Binh <vanbinhdoan971@gmail.com>
2022-03-28 22:47:15 +0700

    first

---
+++ src/main/java/com/example/springtokenbasedauth/SpringTokenBasedAuthApplication.java
@@ -0,0 +1,13 @@
+package com.example.springtokenbasedauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTokenBasedAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringTokenBasedAuthApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springtokenbasedauth/SpringTokenBasedAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springtokenbasedauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTokenBasedAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
